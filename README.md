5024016ea26f901d6fc5ae47a1575162372d7334
Eric Casteleijn <thisfred@gmail.com>
2020-04-24 19:44:38 -0700

    Working cookiecutter template

---
+++ README.md
@@ -0,0 +1,2 @@
+A barebones modern python cookiecutter template.
+
---
+++ {{cookiecutter.project_slug}}/tests/test_import.py
@@ -0,0 +1,10 @@
+import {{ cookiecutter.project_slug }}
+
+
+def test_import():
+    """
+    This is a test that packaging and testing is all set up.
+
+    Once it passes, you should feel free to delete it.
+    """
+    assert {{ cookiecutter.project_slug }}
